695c6cd85b013921c93b6766c7e1788b663a531d
piliptsevich <piliptsevichalex@gmail.com>
2017-07-18 20:04:22 +0200

    Add files via upload

--- js/delay-appearing.js
+++ js/delay-appearing.js
@@ -1,3 +1,10 @@
-setTimeout(function(){
-document.getElementById('delay-appearing').style.visibility = "visible";
-},1000);
+// setTimeout(function(){
+// document.getElementById('delay-appearing').style.visibility = "visible";
+// },1000);
+
+$(function() {
+    $('#delay-appearing').fadeTo(200, 1);
+    $('#fadeIn').click(function(){
+         $('#delay-appearing').fadeIn('slow', function(){alert('Отображено');});
+    });
+});

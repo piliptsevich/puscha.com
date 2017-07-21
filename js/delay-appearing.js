1e67d52c9b09b25f519adba45e98aa1e5251ca69
piliptsevich <piliptsevichalex@gmail.com>
2017-07-21 17:50:51 +0200

    A

--- js/delay-appearing.js
+++ js/delay-appearing.js
@@ -4,7 +4,34 @@
 
 $(function() {
     $('#delay-appearing').fadeTo(200, 1);
-    $('#fadeIn').click(function(){
-         $('#delay-appearing').fadeIn('slow', function(){alert('Отображено');});
-    });
 });
+
+
+// $('body').animate({
+//     scrollTop: 10000
+//     }, 0,
+
+//     (function() {
+//     $('#delay-appearing').fadeTo(200, 1);
+//   });
+// );
+
+// $(function() {
+//     var oTop = $('#delay-appearing').offset().top - window.innerHeight;
+//     $(window).scroll(function(){
+
+//         var pTop = $('body').scrollTop();
+//         console.log( pTop + ' - ' + oTop );   //just for your debugging
+//         if( pTop > oTop ){
+//             start_count();
+//         }
+//     });
+// });
+
+// function start_count(){
+//     // alert('start_count');
+//     //Add your code here
+//     $(function() {
+//     $('#delay-appearing').fadeTo(200, 1);
+//     });
+// }

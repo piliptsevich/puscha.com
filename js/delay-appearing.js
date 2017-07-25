4f34683104d970248fd56ee88e6254e95c548e44
piliptsevich <piliptsevichalex@gmail.com>
2017-07-25 13:24:48 +0200

    upd_25.7.17

--- js/delay-appearing.js
+++ js/delay-appearing.js
@@ -2,36 +2,23 @@
 // document.getElementById('delay-appearing').style.visibility = "visible";
 // },1000);
 
+// $(function() {
+//     $('#delay-appearing').fadeTo(200, 1);
+// });
+
+// $(window).scroll(function() {
+//    var hT = $('#delay-appearing').offset().top,
+//        hH = $('#delay-appearing').outerHeight(),
+//        wH = $(window).height(),
+//        wS = $(this).scrollTop();
+//   if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
+//        $(function() {
+//            $('#delay-appearing').fadeTo(200, 1);
+//        });
+
+//    }
+// });
+
 $(function() {
     $('#delay-appearing').fadeTo(200, 1);
 });
-
-
-// $('body').animate({
-//     scrollTop: 10000
-//     }, 0,
-
-//     (function() {
-//     $('#delay-appearing').fadeTo(200, 1);
-//   });
-// );
-
-// $(function() {
-//     var oTop = $('#delay-appearing').offset().top - window.innerHeight;
-//     $(window).scroll(function(){
-
-//         var pTop = $('body').scrollTop();
-//         console.log( pTop + ' - ' + oTop );   //just for your debugging
-//         if( pTop > oTop ){
-//             start_count();
-//         }
-//     });
-// });
-
-// function start_count(){
-//     // alert('start_count');
-//     //Add your code here
-//     $(function() {
-//     $('#delay-appearing').fadeTo(200, 1);
-//     });
-// }

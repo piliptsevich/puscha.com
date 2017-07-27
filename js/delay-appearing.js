8889252ddd86121efa8888cc1cac908d3b72ea39
piliptsevich <piliptsevichalex@gmail.com>
2017-07-27 11:48:34 +0200

    upd_27.7.17

--- js/delay-appearing.js
+++ js/delay-appearing.js
@@ -4,7 +4,6 @@
 
 // $(function() {
 //     $('#delay-appearing').fadeTo(200, 1);
-<<<<<<< HEAD
 // });
 
 // $(window).scroll(function() {
@@ -23,19 +22,3 @@
 $(function() {
     $('#delay-appearing').fadeTo(200, 1);
 });
-=======
-// });
-
-$(window).scroll(function() {
-   var hT = $('#delay-appearing').offset().top,
-       hH = $('#delay-appearing').outerHeight(),
-       wH = $(window).height(),
-       wS = $(this).scrollTop();
-  if (wS > (hT+hH-wH) && (hT > wS) && (wS+wH > hT+hH)){
-       $(function() {
-           $('#delay-appearing').fadeTo(200, 1);
-       });
-
-   }
-});
->>>>>>> 39d80120ec4e7768cdf73d4338f89488636661aa
